23e4e4d0160a0d99dfc4142cf89004a460f05a23
sophie202205 <sophie6995137@gmail.com>
2022-05-21 19:26:15 +0800

    know onTouchEvent 影片47

--- app/src/main/java/com/example/viewbyself/MyView.java
+++ app/src/main/java/com/example/viewbyself/MyView.java
@@ -5,6 +5,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.annotation.Nullable;
@@ -25,13 +26,24 @@ public class MyView extends View {
         //onDraw()是系統呼叫的，canvas是系統傳的
         super.onDraw(canvas);
         //Paint : 畫筆
-        Paint paint = new Paint();
-        paint.setColor(Color.RED);
+        //Paint paint = new Paint();
+        //paint.setColor(Color.RED);
         //線的粗度
-        paint.setStrokeWidth(40);
+        //paint.setStrokeWidth(40);
         //cx,cy : 圓心座標
-        canvas.drawCircle(0,0,40, paint);
+        //canvas.drawCircle(0,0,40, paint);
         //cx,cy : 線起始座標
-        canvas.drawLine(0,0,400,400, paint);
+        //canvas.drawLine(0,0,400,400, paint);
+    }
+
+    // 摸一下會觸發
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        //event.getX();//座標X
+        //event.getY();//座標Y
+
+        //super.onTouchEvent(event); ->結果回傳false
+        //如果想要摸下去持續偵測，則回傳true
+        return false;
     }
 }
